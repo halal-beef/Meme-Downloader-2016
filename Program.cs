2171a72b9f3bc030690b8d0b63c607de49e7c789
usrDottik <dotcomnose@gmail.com>
2022-01-04 17:19:06 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+using System;
+
+namespace ShitpostingMachine
+{
+    #pragma warning disable CS8602 // Dereference of a possibly null reference.
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Making Dirs...");
+            Directory.CreateDirectory("Shitposs");
+
+            Console.WriteLine("Starting Hell!");
+
+            for (int i = 0; i < 64; i++)
+            {
+                //Download shit on 64 threads 🥶👌
+                Thread x = new(() => GetShitPost());
+                x.Name = "Shitpost bot n" + i;
+                x.IsBackground = true;
+                x.Start();
+            }
+            Console.ReadKey();
+        }
+
+        public static void GetShitPost()
+        {
+            try
+            {
+                while (true)
+                {
+                    var Result = JObject.Parse(
+                        JArray.Parse(
+
+                            new HttpClient()
+
+                            .GetStringAsync("https://reddit.com/r/shitposting/random.json?limit=1").GetAwaiter().GetResult()
+
+                            )[0]["data"]["children"][0]["data"]
+                            .ToString()
+
+                        );
+
+                    string usableName = Result["url"].ToString().Replace('/', '_').Replace(':', '.').Replace('?', '[');
+                    string PathToResult = Environment.CurrentDirectory + @"/Shitposs/" + usableName;
+                    if (!File.Exists(PathToResult))
+                    {
+                        using (FileStream fs = File.Create(PathToResult))
+                        {
+                            HttpClient httpClient = new();
+                            HttpResponseMessage hrm = httpClient.GetAsync(Result["url"].ToString()).GetAwaiter().GetResult();
+
+                            try
+                            {
+                                hrm.EnsureSuccessStatusCode();
+                            }
+                            catch
+                            {
+                                Console.WriteLine("YOU DON'T HAVE INTERNET YOU FOOL!");
+                                Environment.Exit(0);
+                            }
+                            hrm.Content.CopyToAsync(fs).GetAwaiter().GetResult();
+                            fs.Dispose();
+                            fs.Close();
+                        }
+                        Console.WriteLine($"{Thread.CurrentThread.Name}; Downloaded {Result["title"]}");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Reddit rate limited {Thread.CurrentThread.Name}");
+            }
+        }
+    }
+}
+
+
