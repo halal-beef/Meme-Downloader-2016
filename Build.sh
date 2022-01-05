731b71ae1931a2ffb89f2ffb3861ebc7fa46d0d4
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-01-05 15:48:27 +0000

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,6 @@
 # Github workflows thing
 # the size of this program is gonna be huge xd
-ls /home/runner #gotta do this i need to reverse engineer my way to the path
+ls /home/runner/work/Meme-Downloader-2016 #gotta do this i need to reverse engineer my way to the path
 
 dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output "build\\" --arch x64 --os linux -c release --self-contained true # linux build
 dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output "build\\" --arch x86 --os win -c release --self-contained true # win build
