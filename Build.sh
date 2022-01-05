dec1c4364f9b1c42a169ac280eb21238ee3db6ae
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-05 14:06:09 -0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,6 +1,5 @@
 # Github workflows thing
 # the size of this program is gonna be huge xd
-dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output "build\\" --arch x64 --os linux -c release --self-contained true # linux build
 dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output "build\\" --arch x86 --os win -c release --self-contained true # win build
 
 
