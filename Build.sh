ebda850fea20f558ef7f10e16d2a328e41cae111
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-01-05 15:40:45 +0000

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,4 +1,6 @@
 # Github workflows thing
 # the size of this program is gonna be huge xd
+ls /home/runner #gotta do this i need to reverse engineer my way to the path
+
 dotnet publish --output build\ --arch x64 --os linux -c release --self-contained true # linux build
 dotnet publish --output build\ --arch x86 --os win -c release --self-contained true # win build
