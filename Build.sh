c3e5b4c69e4c8b23f654c42430d776d9508a0c0c
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-01-05 15:34:23 +0000

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,5 +1,4 @@
 # Github workflows thing
 # the size of this program is gonna be huge xd
-dotnet restore # get thy dependencies
-dotnet publish --output build\ --framework net6.0 --arch x64 --os linux -c release --self-contained true # linux build
-dotnet publish --output build\ --framework net6.0 --arch x86 --os win -c release --self-contained true # win build
+dotnet publish --output build\ --arch x64 --os linux -c release --self-contained true # linux build
+dotnet publish --output build\ --arch x86 --os win -c release --self-contained true # win build
