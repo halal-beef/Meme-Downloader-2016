c927135ae2067cd53f25e41564ecca5fb43b3133
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-01-05 15:44:11 +0000

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,5 +2,5 @@
 # the size of this program is gonna be huge xd
 ls /home/runner #gotta do this i need to reverse engineer my way to the path
 
-dotnet publish /home/runner/work/SharpBoot/Meme-Downloader-2016/wadpwodk.csproj --output build\ --arch x64 --os linux -c release --self-contained true # linux build
-dotnet publish --output build\ --arch x86 --os win -c release --self-contained true # win build
+dotnet publish "/home/runner/work/SharpBoot/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output build\ --arch x64 --os linux -c release --self-contained true # linux build
+dotnet publish "/home/runner/work/SharpBoot/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output build\ --arch x86 --os win -c release --self-contained true # win build
