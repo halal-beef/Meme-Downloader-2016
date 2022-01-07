ef5480956807e6cecfee79a4c62e5ca9c0e75569
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-07 20:17:08 -0300

    ...

--- Build.bat
+++ Build.bat
@@ -24,7 +24,7 @@ cd Zipper\
 
 mkdir Zipper\build\
 
-dotnet publish "Zipper.csproj" --output "build\" --arch x64 --os win -c release --self-contained true
+dotnet publish "Zipper.csproj" --output build\ --arch x64 --os win -c release --self-contained true
 
 mv build\Zipper.exe ..\build\
 
