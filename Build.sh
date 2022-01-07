2e80bcc32969629541d476cf629b52fedba4b5e3
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-07 13:57:57 -0300

    Release and Debug builds are now published on g...
    ...ithub releases + removed stuff that I used to "try" and fix the debug build not publishing

--- Build.sh
+++ Build.sh
@@ -6,6 +6,3 @@ dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/Meme
 
 # Move debug build
 mv "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build-debug/Meme Downloader 2016.exe" "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build/DEBUG-Meme Downloader 2016.exe"
-
-echo "Build Folder Contents:"
-ls "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build"
