3418383c32fa97e518dc430ceef9077ee10afaaf
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-07 13:39:32 -0300

    Typos Are My Worst Problem.

--- Build.sh
+++ Build.sh
@@ -5,6 +5,6 @@ dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/Meme
 dotnet publish "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/Meme Downloader 2016.csproj" --output "build-debug\\" --arch x64 --os win -c debug --self-contained true # win build
 
 # Move debug build
-mv "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build-debug/Meme Dwonloader 2016.exe" "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build/[DEBUG] Meme Downloader 2016.exe"
+mv "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build-debug/Meme Downloader 2016.exe" "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build/[DEBUG] Meme Downloader 2016.exe"
 
 ls "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build"
