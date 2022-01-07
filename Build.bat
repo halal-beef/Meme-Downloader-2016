f5e067ac4f59d74a095cdbaf862ff464dcd4bab2
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-07 19:00:46 -0300

    Deez isn't Shell, rather Batch

--- Build.bat
+++ Build.bat
@@ -6,9 +6,9 @@ mkdir build\
 mkdir build-debug\
 
 echo 'Building Meme Downloader 2016 x64 debug-release'
-dotnet publish "Meme Downloader 2016.csproj" --output "build\\" --arch x64 --os win -c release --self-contained true # win build
+dotnet publish "Meme Downloader 2016.csproj" --output "build\\" --arch x64 --os win -c release --self-contained true
 
-dotnet publish "Meme Downloader 2016.csproj" --output "build-debug\\" --arch x64 --os win -c debug --self-contained true # win build
+dotnet publish "Meme Downloader 2016.csproj" --output "build-debug\\" --arch x64 --os win -c debug --self-contained true
 
 :: Move debug build
 move \build-debug\Meme Downloader 2016.exe \build\DEBUG-Meme Downloader 2016.exe
