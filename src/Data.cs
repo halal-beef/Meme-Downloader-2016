3aa452ca3c9c8316fde0756d0d058dc8d5c2d3d5
usrDottik <dotcomnose@gmail.com>
2022-01-08 17:49:52 -0300

    Ditch the use of two unnecessary lists made into signed Int64 integers, it should also increase performance and make the Memory usage a bit lower

--- src/Data.cs
+++ src/Data.cs
@@ -37,8 +37,8 @@
     internal struct BotInformation
     {
         public static List<string> BlackListed = new();
-        public static List<bool> aliveBots0 = new();
-        public static List<bool> aliveBots1 = new();
+        public static int aliveBots0 = 0;
+        public static int aliveBots1 = 0;
     }
     internal struct Configurations 
     {
