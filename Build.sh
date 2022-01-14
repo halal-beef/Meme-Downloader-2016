3070cd39d7138f2556cd90442df080f89e22230b
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-14 17:55:55 -0300

    Revert Build.sh changes.

--- Build.sh
+++ Build.sh
@@ -6,11 +6,3 @@ dotnet publish "Meme Downloader 2016.csproj" --output build-debug\ --arch x64 --
 
 # Move debug build
 mv "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build-debug/Meme Downloader 2016.exe" "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build/DEBUG-Meme Downloader 2016.exe"
-
-dotnet publish "Meme Downloader 2016.csproj" --output Linux-Test\ -r linux-x64
-
-cd Linux-Test\
-
-chmod +x Meme\ Downloader\ 2016
-
-./Meme\ Downloader\ 2016
