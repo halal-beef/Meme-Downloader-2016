e8781338d06b2c9d2c62a3b96e009786a65f064e
Dottik <81375555+usrDottik@users.noreply.github.com>
2022-01-14 17:57:46 -0300

    I broke it xd

--- Build.sh
+++ Build.sh
@@ -1,8 +1,8 @@
 # Github workflows thing
 # the size of this program is gonna be huge xd
-dotnet publish "Meme Downloader 2016.csproj" --output build\ --arch x64 --os win -c release --self-contained true # win build
+dotnet publish "Meme-Downloader-2016/Meme Downloader 2016.csproj" --output build\ --arch x64 --os win -c release --self-contained true # win build
 
-dotnet publish "Meme Downloader 2016.csproj" --output build-debug\ --arch x64 --os win -c debug --self-contained true # win build
+dotnet publish "Meme-Downloader-2016/Meme Downloader 2016.csproj" --output build-debug\ --arch x64 --os win -c debug --self-contained true # win build
 
 # Move debug build
 mv "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build-debug/Meme Downloader 2016.exe" "/home/runner/work/Meme-Downloader-2016/Meme-Downloader-2016/build/DEBUG-Meme Downloader 2016.exe"
